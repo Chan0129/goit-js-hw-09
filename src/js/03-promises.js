68b04368ba908c9cc8c642ab81b4489c59040aae
Chan0129 <142906874+Chan0129@users.noreply.github.com>
2024-02-25 22:58:32 +0800

    Tast 3 promises js update

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,24 +1,34 @@
-import Notiflix from 'notiflix';
-
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
     setTimeout(() => {
       if (shouldResolve) {
-        // Resolve
         resolve({ position, delay });
       } else {
-        // Reject
         reject({ position, delay });
       }
     }, delay);
   });
 }
 
-createPromise(2, 1500)
-  .then(({ position, delay }) => {
-    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
+document.querySelector('.form').addEventListener('submit', function (event) {
+  event.preventDefault(); // Prevent form submission
+  const delay = parseInt(this.elements['delay'].value);
+  const step = parseInt(this.elements['step'].value);
+  const amount = parseInt(this.elements['amount'].value);
+
+  // Iterate to create promises
+  for (let i = 0; i < amount; i++) {
+    createPromise(i + 1, delay + step * i)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+  }
+});
